1453c72532b007faca9f5c3bf89ffa2df298abb8
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-10 12:22:51 +0200

    install django rest framework

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path, re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
     re_path("", include("www.urls")),
 ]
