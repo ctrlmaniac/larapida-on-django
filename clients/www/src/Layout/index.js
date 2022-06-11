5915ee5cea6cf8b515c5630905182950cb416814
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-11 16:56:40 +0200

    init Layout

---
+++ clients/www/src/Layout/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  AppBar,
+  Drawer,
+  IconButton,
+  Link,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { IconMenu2 } from "@tabler/icons";
+import { Box } from "@mui/system";
+
+export default function Layout({ children }) {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <AppBar color="inherit" position="fixed" elevation={0}>
+        <Toolbar>
+          <IconButton
+            onClick={() => setOpen(true)}
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <IconMenu2 />
+          </IconButton>
+
+          <Typography variant="body1" component="div" sx={{ flexGrow: 1 }}>
+            <Link href="/" underline="none" color="inherit">
+              <b>La Rapida</b>
+            </Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer anchor="left" open={open} onClose={() => setOpen(false)}>
+        <p>Drawer</p>
+      </Drawer>
+
+      <Box sx={{ pt: "64px" }}>{children}</Box>
+    </React.Fragment>
+  );
+}
