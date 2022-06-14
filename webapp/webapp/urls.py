1155b267460ddf9e8c0113cb122710e099d75345
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-14 21:54:00 +0200

    serve static and media files during development

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls")),
-    path("api/", include("api.urls")),
-    re_path("", include("www.urls")),
-]
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("api-auth/", include("rest_framework.urls")),
+        path("api/", include("api.urls")),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + [
+        re_path("", include("www.urls")),
+    ]
+)
