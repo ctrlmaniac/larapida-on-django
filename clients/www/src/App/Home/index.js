dd6b26987f22a912f40fcf1d2834632cc5fe87d4
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-25 18:10:41 +0200

    add Page to other components

--- clients/www/src/App/Home/index.js
+++ clients/www/src/App/Home/index.js
@@ -1,13 +1,14 @@
+import { Page } from "Components";
 import React from "react";
 import AboveTheFold from "./AboveTheFold";
 import ServiziWidget from "./Servizi";
 
 export default function Home() {
   return (
-    <React.Fragment>
+    <Page>
       <AboveTheFold />
 
       <ServiziWidget />
-    </React.Fragment>
+    </Page>
   );
 }
--- clients/www/src/App/NotFound/index.js
+++ clients/www/src/App/NotFound/index.js
@@ -7,23 +7,25 @@ export default function NotFound() {
   const navigate = useNavigate();
 
   return (
-    <Container>
-      <Box sx={{ textAlign: "center" }}>
-        <img
-          alt="pagina non trovata"
-          src={process.env.PUBLIC_URL + "/animations/not-found.gif"}
-          style={{ maxWidth: "100%" }}
-          width={450}
-          height="auto"
-        />
-        <Typography variant="h6">Qui non c'è niente!</Typography>
-        <Typography gutterBottom>
-          La pagina che stavi cercando non esiste!
-        </Typography>
-        <Button variant="contained" onClick={() => navigate(-1)}>
-          Torna indietro
-        </Button>
-      </Box>
-    </Container>
+    <Page>
+      <Container>
+        <Box sx={{ textAlign: "center" }}>
+          <img
+            alt="pagina non trovata"
+            src={process.env.PUBLIC_URL + "/animations/not-found.gif"}
+            style={{ maxWidth: "100%" }}
+            width={450}
+            height="auto"
+          />
+          <Typography variant="h6">Qui non c'è niente!</Typography>
+          <Typography gutterBottom>
+            La pagina che stavi cercando non esiste!
+          </Typography>
+          <Button variant="contained" onClick={() => navigate(-1)}>
+            Torna indietro
+          </Button>
+        </Box>
+      </Container>
+    </Page>
   );
 }
