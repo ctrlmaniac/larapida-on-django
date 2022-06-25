73885843d2968103e60e888c6067643eb88429b5
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-25 15:29:27 +0200

    fix filtering

--- webapp/magazzino/viewsets/prodotto.py
+++ webapp/magazzino/viewsets/prodotto.py
@@ -3,12 +3,14 @@ from rest_framework import filters, permissions, viewsets
 from ..models.prodotto import Prodotto
 from ..serializers import ProdottoPolymorphicSerializer
 
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class ProdottoViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Prodotto.objects.all()
     serializer_class = ProdottoPolymorphicSerializer
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     search_fields = ["nome", "descrizione_breve", "descrizione"]
     filterset_fields = [
         "categoria",
