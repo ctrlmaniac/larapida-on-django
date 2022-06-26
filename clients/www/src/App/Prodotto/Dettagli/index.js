0090fb26d70b2fc81954682d774fb0f2e9be29d3
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-26 11:06:01 +0200

    add Varianti

--- clients/www/src/App/Prodotto/Dettagli/index.js
+++ clients/www/src/App/Prodotto/Dettagli/index.js
@@ -1,7 +1,8 @@
 import { Typography } from "@mui/material";
 import { Header, Page } from "Components";
 import React from "react";
-import Prezzo from "./Prezzo";
+import Prezzo from "../Prezzo";
+import Varianti from "./Varianti";
 
 export default function Dettagli({ prodotto }) {
   return (
@@ -13,6 +14,10 @@ export default function Dettagli({ prodotto }) {
         <Typography gutterBottom>{prodotto.descrizione_breve}</Typography>
         <Prezzo prodotto={prodotto} />
       </Header>
+
+      {prodotto.varianti.length > 0 && (
+        <Varianti varianti={prodotto.varianti} />
+      )}
     </Page>
   );
 }
