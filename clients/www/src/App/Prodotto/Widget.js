c16ca245c7051961aeee390a0855fc787e367131
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-26 10:13:37 +0200

    add link to prodotto

--- clients/www/src/App/Prodotto/Widget.js
+++ clients/www/src/App/Prodotto/Widget.js
@@ -1,10 +1,16 @@
 import { ButtonBase, Card, CardContent, Typography, Box } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function ProdottoWidget({ prodotto }) {
+  const navigate = useNavigate();
+
   return (
     <Card sx={{ display: "flex" }}>
-      <ButtonBase sx={{ width: "100% !important" }}>
+      <ButtonBase
+        sx={{ width: "100% !important" }}
+        onClick={() => navigate("/" + prodotto.url)}
+      >
         <Box sx={{ display: "flex", flexDirection: "column" }}>
           <CardContent>
             <Typography>
