cd260cca6c809fda83d10b34db4e487652b5151d
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-26 11:38:03 +0200

    add Immagini

---
+++ clients/www/src/App/Prodotto/Dettagli/Immagini.js
@@ -0,0 +1,35 @@
+import { Box, Container } from "@mui/material";
+import { filter } from "lodash";
+import React from "react";
+
+export default function Immagini({ immagini }) {
+  let imgs = filter(immagini, (i) => i.thumbnail === false);
+
+  return (
+    <Container maxWidth="md" sx={{ display: "flex", justifyContent: "center" }}>
+      <Box
+        sx={{
+          display: "flex",
+          maxWidth: "100%",
+          overflowX: "auto",
+          overflowY: "hidden",
+          maxHeight: 280,
+          textAlign: "center",
+          mb: 3,
+          mt: 3,
+        }}
+      >
+        {imgs.map((imm, i) => {
+          return (
+            <img
+              key={i}
+              alt="Immagine"
+              src={imm.file}
+              style={{ maxHeight: 300 }}
+            />
+          );
+        })}
+      </Box>
+    </Container>
+  );
+}
--- clients/www/src/App/Prodotto/Dettagli/index.js
+++ clients/www/src/App/Prodotto/Dettagli/index.js
@@ -3,6 +3,7 @@ import { Header, Page } from "Components";
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import Prezzo from "../Prezzo";
+import Immagini from "./Immagini";
 import Varianti from "./Varianti";
 
 export default function Dettagli({ prodotto }) {
@@ -15,6 +16,10 @@ export default function Dettagli({ prodotto }) {
         <Typography gutterBottom>{prodotto.descrizione_breve}</Typography>
         <Prezzo prodotto={prodotto} />
       </Header>
+
+      {prodotto.immagini.length > 0 && (
+        <Immagini immagini={prodotto.immagini} />
+      )}
 
       {prodotto.varianti.length > 0 && (
         <Varianti varianti={prodotto.varianti} />
