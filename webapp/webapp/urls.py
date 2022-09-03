c236e74151a10a7da524887d32c739b4c257e1c6
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-09-03 17:57:00 +0200

    add static and media url

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 
@@ -6,6 +8,10 @@ urlpatterns = (
         path("admin/", admin.site.urls),
         path("api-auth/", include("rest_framework.urls")),
         path("api/", include("api.urls")),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + [
         re_path("", include("www.urls")),
     ]
 )
