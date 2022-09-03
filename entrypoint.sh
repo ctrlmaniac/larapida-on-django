43e05f4d58d795144d3384bdaffb403ae00f1594
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-09-03 15:33:26 +0200

    init entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+cd /home/webapp
+
+poetry run python manage.py migrate
+poetry run python manage.py collectstatic --no-input --clear
+poetry run gunicorn -w 2 -b :8000 webapp.wsgi:application
