7f6456041c5a10e7ebb1681fe6bd4d6d89963f58
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-09-03 16:25:02 +0200

    wait for postgres to start

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
 cd /home/webapp
 
 poetry run python manage.py migrate
