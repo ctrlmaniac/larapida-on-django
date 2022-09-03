1b689b7f84f18ac4e0c03081ec52de8c3bdfda7d
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-09-03 21:58:13 +0200

    move migrate at the bottom

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,6 @@ fi
 
 cd /home/webapp
 
-poetry run python manage.py migrate
 poetry run python manage.py collectstatic --no-input --clear
+poetry run python manage.py migrate
 poetry run gunicorn -w 2 -b :8000 webapp.wsgi:application
