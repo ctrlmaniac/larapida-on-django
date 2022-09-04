d3db49d173a94b6d907e6a2cc9dc57d77d46bee7
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-09-04 14:48:07 +0200

    change where the database is created in development

--- webapp/webapp/development.py
+++ webapp/webapp/development.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = "webapp.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "data/db.sqlite3",
+        "NAME": ROOT_DIR / "db.sqlite3",
     }
 }
 
