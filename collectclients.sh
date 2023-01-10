10897ae4f98be83be80c5a52687511f02035783c
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-01-10 20:57:06 +0100

    remove static and template folder

--- collectclients.sh
+++ collectclients.sh
@@ -3,20 +3,22 @@
 cd clients
 
 for client in *; do
+
     # Delete old directories
     if [ -d ../webapp/$client/templates/$client ]; then
         rm -rf ../webapp/$client/templates/$client
     fi
 
-    if [ -d ../webapp/$client/static/$client ]; then
-        rm -rf ../webapp/$client/static/$client
+    if [ -d ../webapp/$client/static ]; then
+        rm -rf ../webapp/$client/static
     fi
 
     # Create directories
     mkdir -p ../webapp/$client/templates/$client
-    mkdir -p ../webapp/$client/static/$client
 
     # Copy files
-    cp -r $client/build/index.html ../webapp/$client/templates/$client/index.html
-    cp -r $client/build/static/ ../webapp/$client/static/$client/
+    cp -r $client/dist/index.html ../webapp/$client/templates/$client/index.html
+    cp -r $client/dist/static ../webapp/$client
+
 done
+
