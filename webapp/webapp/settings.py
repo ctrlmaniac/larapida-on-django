057a2cbcca538f67bc12f5af21637ee1a5d891fa
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-03-09 19:59:34 +0100

    add settings for django rest_framework

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "utils",
     "account",
     "business",
@@ -118,3 +119,11 @@ MEDIA_ROOT = ROOT_DIR / "data/media"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+}
