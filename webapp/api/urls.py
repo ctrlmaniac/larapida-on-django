e88e2e81608b97951d0a6233055e35c7f8431b7e
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-03-09 20:36:15 +0100

    add endpoints for stores

--- webapp/api/urls.py
+++ webapp/api/urls.py
@@ -3,10 +3,11 @@ from django.urls import include, path
 
 from business.viewsets import BusinessViewSet
 from business.api_views import BusinessPrincipalApiView
-
+from store.viewsets import StoreViewSet
 
 router = routers.DefaultRouter()
 router.register(r"business", BusinessViewSet)
+router.register(r"stores", StoreViewSet)
 
 
 urlpatterns = [
---
+++ webapp/store/viewsets/__init__.py
@@ -0,0 +1,3 @@
+from .store import StoreViewSet
+
+__all__ = ["StoreViewSet"]
