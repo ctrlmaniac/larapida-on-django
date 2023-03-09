188e4ff1f5937e0c7669865f3fbd53333d55cc20
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-03-09 21:31:24 +0100

    install all deps of client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const commonConfigs = {
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "src"),
+    },
+  },
+};
+
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return {
+      ...commonConfigs,
+      server: {
+        proxy: {
+          "/api": {
+            target: "http://127.0.0.1:8080",
+          },
+        },
+      },
+    };
+  } else {
+    return {
+      ...commonConfigs,
+      build: {
+        rollupOptions: {
+          output: {
+            assetFileNames: (assetInfo) => {
+              let extType = assetInfo.name.split(".").at(1);
+              if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                extType = "img";
+              }
+              return `static/www/${extType}/[name]-[hash][extname]`;
+            },
+            chunkFileNames: "static/www/js/[name]-[hash].js",
+            entryFileNames: "static/www/js/[name]-[hash].js",
+          },
+        },
+      },
+    };
+  }
+});
