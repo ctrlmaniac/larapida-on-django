4232df9f3258d2bfa4505a4a73fc39b06b0398d3
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-03-09 20:38:02 +0100

    add permission classes

--- webapp/business/viewsets/business.py
+++ webapp/business/viewsets/business.py
@@ -1,9 +1,12 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from ..models import Business
 from ..serializers import BusinessSerializer
 
 
 class BusinessViewSet(viewsets.ModelViewSet):
+    """Only authenticated users will be allowed to see the full list of businesses"""
+
     queryset = Business.objects.all()
     serializer_class = BusinessSerializer
+    permission_classes = [permissions.IsAuthenticated]
